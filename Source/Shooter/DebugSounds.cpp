7c6f82cd384fd867ce3d3eb164318b1fbac61118
JoaoRoch4 <joao.andre.rocha.de.almeida@gmail.com>
2023-11-06 05:37:51 -0400

    Nova classe para emitir sons

---
+++ Source/Shooter/DebugSounds.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DebugSounds.h"
+
+// Sets default values
+ADebugSounds::ADebugSounds()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ADebugSounds::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADebugSounds::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
