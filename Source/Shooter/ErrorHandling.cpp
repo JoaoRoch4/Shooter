93f9d7d26e970bb5a2cd8f218ca09d546005ea9e
joao almeida <jubarochaalmeida@hotmail.com>
2024-10-11 18:59:00 -0400

    Add phase

---
+++ Source/Shooter/ErrorHandling.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ErrorHandling.h"
+
+// Sets default values
+AErrorHandling::AErrorHandling()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AErrorHandling::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AErrorHandling::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
